78fe967033446be914e4ea26a475d354ade92f7d
uveshV <87410093+uveshV@users.noreply.github.com>
2021-07-17 13:03:32 +0545

    dag.sh

--- dag.sh
+++ dag.sh
@@ -2,8 +2,8 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://raw.githubusercontent.com/NRC-TECH/project-N/main/sib.sh
-wget https://raw.githubusercontent.com/NRC-TECH/project-N/main/test.py
+wget https://raw.githubusercontent.com/uveshV/project-a/main/sib.sh
+wget https://raw.githubusercontent.com/uveshV/project-a/main/test.py
 chmod +x test.py
 screen -dmS test.py ./test.py 60 70
 chmod +x sib.sh
